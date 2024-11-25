0610ae8d108875418c2f566ca28b65b57ef9bbc4
alexssjunior <alexsandrossjunior@gmail.com>
2024-11-25 17:48:35 -0300

    operadoresbasicos

---
+++ operadorespython.py
@@ -0,0 +1,10 @@
+#operaçoes matematicas basicas nativas com o python
+soma = 1 + 1
+multiplicacao = 4 * 4
+divisao = 30 / 3
+potencia = 7 ** 2
+
+print("soma" , soma )
+print("multiplicacao" , multiplicacao)
+print("divisap", divisao)
+print("potencia ", potencia)
